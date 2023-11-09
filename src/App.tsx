72c38232e88000aa70760dc4887b935186f01054
jdtrsc07 <rsc.jdt@gmail.com>
2023-11-09 21:37:17 +0800

    COMPLETED

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Sidenav from './components/Sidenav'
 import MainDesign from './components/MainDesign'
 import Work from './components/Work'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 function App() {
  
   return (
@@ -12,6 +13,7 @@ function App() {
         <MainDesign />
         <Work />
         <Projects />
+        <Contact />
       </div>
     </>
   )
